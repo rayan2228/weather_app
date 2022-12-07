e1150d4b891e67c5bb236364fe48dc5e563b7441
rayan <rhrayan2228@gmail.com>
2022-12-07 17:44:23 +0600

    add searching aption and add dynamic data

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ const cityNameElement = document.querySelector("#cityName");
 const cityCodeElement = document.querySelector("#cityCode");
 const localDateElement = document.querySelector("#localDate");
 const localTimeElement = document.querySelector("#localTime");
-const refreshButtonElement = document.querySelector("#refreshButtonElement");
-const weatherImgElement = document.querySelector(".weather_img");
+const weatherImgElement = document.querySelector("#weather_img");
 const mainTemperatureElement = document.querySelector("#mainTemperature");
 const tempDescriptionElement = document.querySelector("#tempDescription");
 const celsiusElement = document.querySelector("#celsius");
 const fahrenheitElement = document.querySelector("#fahrenheit");
 const humidityElement = document.querySelector("#humidity");
 const windElement = document.querySelector("#wind");
-const mainTempHotElement = document.querySelector("#mainTempHot");
-const mainTempLowElement = document.querySelector("#mainTempLow");
+const pressureElement = document.querySelector("#pressure");
+const feelLikeElement = document.querySelector("#feelLike");
 
 // initial data
 const apiKey = "126688fe7925400d81763c4bb6265bba";
-const cityName = "riyad";
+const cityName = "dhaka";
 const dayList = [
   "Sunday",
   "Monday",
@@ -48,31 +47,46 @@ const setTimeAndDate = () => {
   let month = monthList[dateObj.getMonth()];
   let date =
     dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : dateObj.getDate();
-    let year = dateObj.getFullYear();
+  let year = dateObj.getFullYear();
   localDateElement.innerHTML = `${day} ${month} ${date} ${year}`;
   return function setTime() {
-    let hour = dateObj.getHours() % 12;
+    let hour = dateObj.getHours() % 12 === 0 ? "12" : dateObj.getHours() % 12;
     let minute = dateObj.getMinutes();
     let seconds = dateObj.getSeconds();
     let am_pm = hour > 12 ? "am" : "pm";
-    return localTimeElement.innerHTML = `${hour}:${minute}:${seconds} ${am_pm}`;
+    return (localTimeElement.innerHTML = `${hour}:${minute}:${seconds} ${am_pm}`);
   };
 };
 setInterval(() => {
   setTimeAndDate()();
 }, 1000);
-
+// call api
 let linkApi = async (cityName, apiKey) => {
-  let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
-  );
-  return await response.json();
+
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
+    );
+    return await response.json();
 };
 
 let waitForResponse = async (cityName, apiKey) => {
   let getData = await linkApi(cityName, apiKey);
-  return getData;
+  cityCodeElement.innerText = getData.sys.country;
+  humidityElement.innerText = getData.main.humidity;
+  windElement.innerText = getData.wind.speed;
+  pressureElement.innerText = getData.main.pressure;
+  tempDescriptionElement.innerText = getData.weather[0].main;
+  // feelLikeElement.innerText = getData.main.feels_like;
+  // mainTemperatureElement.innerText = Math.round(getData.main.temp);
+  weatherImgElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/w/${getData.weather[0].icon}.png`
+  );
+
+
+  console.log(getData);
 };
 
-let result = waitForResponse(cityName, apiKey);
-result.then((data) => console.log(data.weather[0]));
+waitForResponse(cityName, apiKey).catch((err) => err);
+
+// console.log(a);
